4f0e0f3dbeb4d87d3b27e24e5ea1afd18b7b9122
Aziza310 <80085094+Aziza310@users.noreply.github.com>
2021-03-05 08:29:21 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html style="background: white url('https://thewallpaper.co/wp-content/uploads/2020/09/abstract-wallpaper-hd-artworks-cool-desktop-images-art-colorful-backgrounds-cute-background-images-lovely-1697x1100-1.jpg')">
+	
+<head>
+	<title>aziza</title>
+<style>
+img {
+  display: block;
+  margin-left: auto;
+ 
+}
+
+
+
+</style>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.fa {
+  padding: 20px;
+  font-size: 30px;
+  width: 50px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+}
+
+
+.fa-youtube {
+  background: #bb0000;
+  color: white;
+}
+
+.fa-instagram {
+  background: #125688;
+  color: white;
+}
+
+
+</style>
+</head>
+<body>
+	<h1 style="color:white"><center>Hello, I'm Aziza!</center></h1>
+
+
+	<p><i style="color:#E0E0E0">Web developer from Bishkek.</i></p>
+	<p><i style="color:#E0E0E0">Welcome to my Portfolio.</i> </p> 
+    <p><i style="color:#E0E0E0">I create custom websites  to help busnisses do better online.</i></p>
+	
+	<button type="button" onclick="alert('Hello world!')">My projects</button>
+	<img src="man.jpg">
+	
+	<p> .</p>
+	<p> .</p>
+	<p> .</p>
+	<p> .</p>
+  <?php echo "" ?>
+
+<h2 style="color: white">Me in social media</h2>
+<a href="https://youtube.com/channel/UCVhTYda6nxDhOIZ02TABHRg" class="fa fa-youtube"></a>
+<a href="https://instagram.com/__dzhamangulova77__?igshid=1hbwwstdylu7b" class="fa fa-instagram"></a>
+
+
+
+
+
+</body>
+</html>
