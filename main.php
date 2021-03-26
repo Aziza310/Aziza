ef7833ebe19675e10eb8cb92f76329aa52256571
Aziza310 <80085094+Aziza310@users.noreply.github.com>
2021-03-26 13:52:32 +0300

    Update main.php

--- main.php
+++ main.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html style="background: url(https://images.squarespace-cdn.com/content/v1/534eb383e4b013cdd4cd17a8/1398471158153-1S09IRJODCDYTDTODGPY/ke17ZwdGBToddI8pDm48kPhE_b-FKx_EcxRx4teFEVN7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0k5fwC0WRNFJBIXiBeNI5fL8LmMajxsBXeYxVzkYts3ds68Ud4HgM4ArFxmxGpI5hQ/background-final_web.jpg?format=2500w);"> 
+<html style="background: url(https://cdn.hipwallpaper.com/i/91/89/RK4kPX.png);"> 
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -28,9 +28,9 @@ h2{
 }
 </style>
 <body>
-<h2><center>HI, WELCOME TO MY PORTFOLIO!</center></h2>
+<h2><center>THIS IS MY PORTFOLIO!</center></h2>
 <h3>Web developer from Bishkek!</h3>
-<h3>Name: AIDANA AKMATOVA</h3>
+<h3>Name: Aziza Dzhamangulova</h3>
 <h3>Age: 19</h3>
 <p>.</p>
 <p>.</p>
